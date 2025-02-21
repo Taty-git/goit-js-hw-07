af9ac1c1fd4277d0ecf363265779c664017e690e
Taty <taty.bystrova@gmail.com>
2025-02-21 17:21:39 +0200

    made task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeClr = document.querySelector('.change-color');
+changeClr.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  document.querySelector('.color').textContent = randomColor;
+});
+
